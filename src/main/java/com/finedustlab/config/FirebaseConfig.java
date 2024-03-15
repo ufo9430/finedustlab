772f3f8f54f011c8134d1ead1e6d8b3d299334b9
ufo9430 <ufo9430@naver.com>
2024-03-16 00:42:53 +0900

    240316-3

--- src/main/java/com/finedustlab/config/FirebaseConfig.java
+++ src/main/java/com/finedustlab/config/FirebaseConfig.java
@@ -4,6 +4,7 @@ import com.google.auth.oauth2.GoogleCredentials;
 import com.google.firebase.FirebaseApp;
 import com.google.firebase.FirebaseOptions;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.ClassPathResource;
 
 import javax.annotation.PostConstruct;
 import java.io.FileInputStream;
@@ -14,8 +15,9 @@ public class FirebaseConfig {
     @PostConstruct
     public void init(){
         try{
+            ClassPathResource res = new ClassPathResource("/key/serviceAccountKey.json");
             FileInputStream serviceAccount =
-                    new FileInputStream("src/main/resources/key/serviceAccountKey.json");
+                    new FileInputStream(res.getFile());
 
             FirebaseOptions options = new FirebaseOptions.Builder()
                     .setCredentials(GoogleCredentials.fromStream(serviceAccount))
